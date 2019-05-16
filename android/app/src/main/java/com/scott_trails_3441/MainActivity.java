285e16111d06d55887ff4dbd178d826d670937e2
Crowdbotics <team@crowdbotics.com>
2019-05-16 17:49:01 +0000

    initial commit

---
+++ android/app/src/main/java/com/scott_trails_3441/MainActivity.java
@@ -0,0 +1,15 @@
+package com.scott_trails_3441;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "scott_trails_3441";
+    }
+}
